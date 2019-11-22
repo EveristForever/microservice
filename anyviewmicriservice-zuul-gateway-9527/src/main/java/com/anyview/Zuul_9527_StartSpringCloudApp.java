152b68ec768aead17cf7274a426abc605ada30b3
admin <admin@mail.com>
2019-11-22 18:09:13 +0800

    版本修改1.0

--- anyviewmicriservice-zuul-gateway-9527/src/main/java/com/anyview/Zuul_9527_StartSpringCloudApp.java
+++ anyviewmicriservice-zuul-gateway-9527/src/main/java/com/anyview/Zuul_9527_StartSpringCloudApp.java
@@ -1,11 +1,12 @@
 package com.anyview;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@SpringBootApplication
 @EnableZuulProxy
+@EnableAutoConfiguration
 public class Zuul_9527_StartSpringCloudApp {
     public static void main(String[] args) {
         SpringApplication.run(Zuul_9527_StartSpringCloudApp.class,args);
